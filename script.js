2edfd0b7a8ff4b164c16b3836ec557d3cd18b0ad
Bruno de Oliveira <brunind2006@gmail.com>
2026-03-23 22:42:47 -0300

    feat: adiciona estrutura inicial do JavaScript com classe ToDo e lógica de criação de tarefas

---
+++ script.js
@@ -0,0 +1,55 @@
+const submit = document.querySelector("#criar-tarefa");
+const input = document.querySelector("#nome-tarefa");
+const form = document.querySelector("#form");
+
+class ToDo {
+  constructor() {
+    this.lista = document.querySelector(".lista-de-tarefas");
+    this.input = document.getElementById("nome-tarefa");
+    this.form = document.getElementById("form");
+
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.adicionarTarefa();
+    });
+
+    this.lista.addEventListener("click", (e) => {
+      this.handleClick(e);
+    });
+  }
+
+  adicionarTarefa() {
+    const texto = this.input.value.trim();
+
+    if (texto == "") return;
+
+    const tarefa = document.createElement("div");
+    tarefa.classList.add("tarefa");
+
+    tarefa.innerHTML = `
+          <label class="checkbox">
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <p class="tarefa-p">${texto}</p>
+
+          <div class="icons">
+            <button class="editar">
+              <i
+                class="fa-solid fa-pen-to-square"
+                style="color: rgb(33, 64, 175)"
+              ></i>
+            </button>
+            <button class="remover">
+              <i class="fa-solid fa-trash" style="color: rgb(33, 64, 175)"></i>
+            </button>
+          </div>`;
+
+    this.lista.appendChild(tarefa);
+
+    this.input.value = "";
+  }
+}
+
+new ToDo();
